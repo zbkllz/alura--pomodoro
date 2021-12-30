7008a6c739eeec9b7775820c9f692e25e4ca66df
Larissa <larissasousa.wrks@gmail.com>
2021-12-30 16:39:57 -0400

    new features

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Item from './Item'
 import style from './Lista.module.scss'
 
 function List() {
-  let tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: 'React',
       time: '02:00:00'
@@ -16,16 +16,20 @@ function List() {
       task: 'TypeScript',
       time: '03:00:00'
     }
-  ]
+  ])
   return (
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {tasks =[...tasks, {task: "Estudar estado", time:"05:00:00"}]}}>Estudos do Dia</h2>
+      <h2
+        onClick={() => {
+          
+          setTasks ([...tasks, { task: 'Estudar estado', time: '05:00:00' }])
+        }}
+      >
+        Estudos do Dia
+      </h2>
       <ul>
         {tasks.map((item, index) => (
-          
-          <Item 
-          key={index}
-          {...item}/>
+          <Item key={index} {...item} />
         ))}
       </ul>
     </aside>
